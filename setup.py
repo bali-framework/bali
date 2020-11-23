b074590aac2f07562c8567d075bbdcda1b26fe83
JoshYuJump <josh.yu_8@live.com>
2020-11-23 17:09:42 +0800

    Fixed lost package `pydantic-sqlalchemy`

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ INSTALL_REQUIREMENTS = [
     'passlib[bcrypt]==1.7.2',
     'pillow==7.2.0',
     'python-jose[cryptography]==3.2.0',
+    'pydantic-sqlalchemy==0.0.7',
     'redis==3.5.3',
     'SQLAlchemy==1.3.19',
     'sqla-wrapper==4.200628',
