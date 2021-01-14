9c20d25fe27e6ee66bcc29324a8548951d3655de
JoshYuJump <josh.yu_8@live.com>
2021-01-14 17:30:32 +0800

    Added gzip route class

--- bali/application.py
+++ bali/application.py
@@ -1,13 +1,37 @@
+import gzip
 from multiprocessing import Process
+from typing import Callable
 
 import typer
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
 from ._utils import singleton
 from .middlewares import process_middleware
-from fastapi.middleware.gzip import GZipMiddleware
+
+
+class GzipRequest(Request):
+    async def body(self) -> bytes:
+        if not hasattr(self, "_body"):
+            body = await super().body()
+            if "gzip" in self.headers.getlist("Content-Encoding"):
+                body = gzip.decompress(body)
+            self._body = body
+        return self._body
+
+
+class GzipRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            request = GzipRequest(request.scope, request.receive)
+            return await original_route_handler(request)
+
+        return custom_route_handler
 
 
 @singleton
@@ -59,6 +83,8 @@ class Bali:
     def http(self):
         """load FastAPI to instance"""
         self._app = FastAPI(**self.base_settings)
+        self._app.router.route_class = GzipRoute
+
         # routers
         for router in self.kwargs.get('routers', []):
             self._app.include_router(**router)
