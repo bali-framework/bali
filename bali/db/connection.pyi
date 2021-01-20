6504af5d1444eee461be5a5802d2e47d50316e3c
Ed-XCF <m.tofu@qq.com>
2021-01-20 20:17:39 +0800

    db stub file

---
+++ bali/db/connection.pyi
@@ -0,0 +1,58 @@
+from datetime import datetime
+from typing import Dict, Optional, Any, Type, TypeVar, Union
+
+from sqlalchemy.orm.query import Query
+from sqlalchemy.orm.session import Session
+from sqlalchemy.schema import Column
+from sqla_wrapper import SQLAlchemy
+
+_M = TypeVar("_M")
+_DateTimeField = Union[datetime, Column[Optional[datetime]]]
+_BooleanField = Union[bool, Column[Optional[bool]]]
+
+
+class BaseModel:
+    __abstract__ = True
+    created_time: _DateTimeField
+    updated_time: _DateTimeField
+    is_active: _BooleanField
+
+    def to_dict(self) -> Dict[str, Any]: ...
+
+    @classmethod
+    def exists(cls: Type[_M], **attrs) -> bool: ...
+
+    @classmethod
+    def create(cls: Type[_M], **attrs) -> _M: ...
+
+    @classmethod
+    def create_or_first(cls: Type[_M], **attrs) -> _M: ...
+
+    @classmethod
+    def first(cls: Type[_M], **attrs) -> Optional[_M]: ...
+
+    @classmethod
+    def first_or_error(cls: Type[_M], **attrs) -> _M:
+        """
+        raise: ValueError
+        """
+        ...
+
+    @classmethod
+    def query(cls: Type[_M]) -> Query: ...
+
+    def save(self) -> _M: ...
+
+    def delete(self) -> None: ...
+
+
+class DB(SQLAlchemy):
+    BaseModel: BaseModel
+
+    def connect(self, database_uri: str) -> None: ...
+
+    @property
+    def session(self) -> Session: ...
+
+
+db = DB()
