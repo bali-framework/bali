074a1bd09597e887a873d68d94f0931c842d3957
Ed-XCF <m.tofu@qq.com>
2021-02-20 17:24:51 +0800

    timezone.now use UTC

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,25 @@
 from datetime import datetime
-from bali.utils import dateparser
+
+from bali import timezone
+from bali.utils import dateparser, get_beginning_datetime
 
 
 def test_parse_dmy_date():
     s = '04/03/2014 22:07:40'
     expect = datetime(2014, 3, 4, 22, 7, 40)
     assert dateparser.parse(s, settings={'DATE_ORDER': 'DMY'}) == expect
+
+
+def test_get_year_beginning_datetime():
+    d = get_beginning_datetime(year=2000)
+    assert timezone.make_naive(d) == datetime(2000, 1, 1)
+
+
+def test_get_month_beginning_datetime():
+    d = get_beginning_datetime(year=2000, month=2)
+    assert timezone.make_naive(d) == datetime(2000, 2, 1)
+
+
+def test_get_day_beginning_datetime():
+    d = get_beginning_datetime(year=2000, month=2, day=3)
+    assert timezone.make_naive(d) == datetime(2000, 2, 3)
