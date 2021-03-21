3329d7cfeb41d51ca83428457b078e421f5b2b0c
JoshYuJump <josh.yu_8@live.com>
2021-03-21 22:13:46 +0800

    Fixed router name

--- bali/routing.py
+++ bali/routing.py
@@ -2,6 +2,7 @@ import inspect
 from typing import Callable
 
 from fastapi import APIRouter as FastAPIRouter
+from starlette.routing import get_name
 
 from bali.core import db
 
@@ -19,5 +20,7 @@ class APIRouter(FastAPIRouter):
     def add_api_route(self, path: str, endpoint: Callable, *args, **kwargs):
         # inject sqlalchemy scope session remove to endpoint
         if not inspect.iscoroutinefunction(endpoint):
+            name = kwargs.get('name')
+            kwargs.update(name=get_name(endpoint) if name is None else name)
             endpoint = self._inject_scoped_session_clear(endpoint)
         return super().add_api_route(path, endpoint, *args, **kwargs)
