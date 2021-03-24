0807dc864bf985bcd67bb5a0b0cf061b864afcd1
JoshYuJump <josh.yu_8@live.com>
2021-03-24 11:00:42 +0800

    Changed the example

--- examples/models.py
+++ examples/models.py
@@ -4,5 +4,5 @@ from bali.core import db
 class Item(db.Model):
     __tablename__ = "item"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), default='')
