98036c55968ba238a04f4b9742bfb4294f409a4e
JoshYuJump <josh.yu_8@live.com>
2022-05-20 18:01:47 +0800

    Added tests: test queue declared in event handler started

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,10 +1,14 @@
 import os
 
+import amqp
+import pytest
+from kombu import Connection, Queue, Exchange
+
 from bali.core import _settings
 from bali.decorators import event_handler
 from bali.events import Event, dispatch, handle
 
-_settings.AMQP_CONFIGS = {
+_settings.AMQP_CONFIGS  = {
     'default':
         {
             'AMQP_SERVER_ADDRESS':
@@ -65,3 +69,45 @@ def test_event_handler(mocker):
     handle()
     os.path.basename.assert_called_with('aaa.txt')
     os.path.dirname.assert_called_with('bbb.txt')
+
+
+def test_queue_declared_in_event_handler(mocker):
+    # 1. Define a `Product` service
+    service_abbr = 'product'
+    uri = os.getenv('AMQP_SERVER_ADDRESS', default='amqp://127.0.0.1:5672')
+    _settings.AMQP_CONFIGS = {
+        'default': {
+            'AMQP_SERVER_ADDRESS': uri,
+            'EXCHANGE_NAME': 'ms.events',
+            'EXCHANGE_TYPE': 'fanout',
+            'QUEUE_NAME': f'{service_abbr}.events',
+        }
+    }
+    # 2. Ensure RabbitMQ has no queue for `Product` service
+    amqp_server_address = 'amqp://127.0.0.1:5672'
+    conn = Connection(amqp_server_address)
+    channel = conn.channel()
+    exchange = Exchange('ms.events', type='fanout')
+    queue_name = f'{service_abbr}.events'
+
+    b = Queue(queue_name, exchange, queue_name, channel=channel)
+    with pytest.raises(amqp.exceptions.NotFound):
+        b.queue_declare(passive=True)
+
+    # 3. Launch `Product` service's handle
+    from bali.decorators import event_handler
+
+    @event_handler('UserCreated')
+    def handle_product_created(event):
+        pass
+
+    handle()
+    # 4. Assert `product.events` queue exists，and bind to exchange `ms.events`
+    b = Queue(queue_name, exchange, queue_name, channel=channel)
+    assert b.queue_declare(passive=True)
+    # assert 'exchange_declare' in channel
+    # assert 'queue_declare' in channel
+    # assert 'queue_bind' in channel
+
+    # clean
+    conn.close()
