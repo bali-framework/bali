8a3a17db2b2989dc45f8810076a2d24ad47ee5ee
JoshYuJump <josh.yu_8@live.com>
2022-06-15 16:47:36 +0800

    Expose Manager and AsyncManager

--- bali/__init__.py
+++ bali/__init__.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 from bali.core import APIRouter, Bali, db, cache, cache_memoize
+from bali.db.managers import Manager, AsyncManager
 from bali.resources import Resource, ModelResource
 
 __version__ = '3.2.0'
