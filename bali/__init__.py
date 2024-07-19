de1c920552c5cbec4773589988345ae0825e0237
Josh Yu <josh.yu_8@live.com>
2024-07-19 11:09:49 +0800

    Bump version 3.5.2

--- bali/__init__.py
+++ bali/__init__.py
@@ -5,7 +5,7 @@ from bali.db.managers import Manager, AsyncManager
 from bali.decorators import event_handler, init_handler
 from bali.resources import Resource, ModelResource
 
-__version__ = '3.5.1'
+__version__ = '3.5.2'
 
 
 class Schema(BaseModel):
